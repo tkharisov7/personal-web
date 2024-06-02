b67c67922ef87c35bd455c42884bf8ef0f94e298
Timur Kharisov <tkharisov7@gmail.com>
2024-06-02 19:23:16 +0300

    Add footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,10 +14,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={`${inter.className} flex flex-col min-h-screen`}>
 				<DarkModeProvider>
 					<NavLinks />
 					{children}
+					<footer className="footer">
+						<p>© 2024 Timur Kharisov. Website code is available at <a href="github.com/tkharisov7/personal-website" className="text-blue-500">GitHub</a>.</p>
+						<p>This site was created using Next.js, Tailwind CSS, and React, and is hosted on Vercel.</p>
+					</footer>
 				</DarkModeProvider>
 			</body>
 		</html>
